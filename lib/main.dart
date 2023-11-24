42c52d03fa3a529f38f4740db1482970a790097d
Vincent Schult <vschult@student.ethz.ch>
2023-11-24 17:46:53 +0100

    Code Grundgeruest

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,164 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Habit Tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HabitTrackerPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+//Class Defining what a Habit is
+class Habit {
+  String title;
+  String description;
+  int value;
+  IconData iconData;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Habit({required this.title, required this.description, required this.value, required this.iconData});
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HabitTrackerPage extends StatefulWidget {
+  @override
+  _HabitTrackerPageState createState() => _HabitTrackerPageState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+//Main Page with the home menu and everything. Also includes the Add new habit dialog
+class _HabitTrackerPageState extends State<HabitTrackerPage> {
+
+  //Variable needed to update the icon in the add new dialog, since it doesn't handle state updates in the showDialog
+  IconData? _selectedIcon;
+
+  //Define the list of available icons to choose from
+  final List<IconData> availableIcons = [
+    Icons.fitness_center,
+    Icons.book,
+    Icons.music_note,
+    Icons.work,
+    // ... add more, potentially also custom icons and icons not from the Icons class
+  ];
+  // List storing all the user habits
+  final List<Habit> _habits = [];
+
+  //Self-explanatory: Add a new habit to the list
+  void _addHabit(String title, String description, IconData icon) {
+    if(title.isNotEmpty){
+      setState(() {
+        _habits.add(Habit(title: title, description: description, value: 0, iconData: icon));
+      });
+    }
   }
 
+  //Render the stuff in the home menu
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Habit Tracker'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+
+      //List with all the habits
+      body: ListView.builder(
+        itemCount: _habits.length,
+        itemBuilder: (context, index) {
+
+          //Cards are clickable!
+          return Card(
+            elevation: 4.0, //Shadow effect
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: InkWell(
+              onTap: () {
+                // Handle the tap event
+                print('Habit Pressed: ${_habits[index].title}');
+                setState(() {
+                  _habits[index].value++;
+                });
+              },
+              child: ListTile(
+                leading: Icon(_habits[index].iconData),
+                title: Text(_habits[index].title),
+                subtitle: Text("Count: ${_habits[index].value}"),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          );
+        },
       ),
+
+      //Add new Habit button
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _showAddHabitDialog,
+        child: Icon(Icons.add),
+      ),
     );
   }
+
+  //Add new Habit dialog
+  Future<void> _showAddHabitDialog() async{ //The Dialog for adding a new habit
+    TextEditingController titleController = TextEditingController();
+    IconData selectedIcon = availableIcons.first;
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setStateDialog){
+            return AlertDialog(
+              title: Text('Add New Habit'),
+              content: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(labelText: 'Title'),
+                    ),
+                    DropdownButton<IconData>(
+                      value: _selectedIcon ?? availableIcons.first,
+                      onChanged: (IconData? newValue) {
+                        selectedIcon = newValue!;
+                        setStateDialog(() => _selectedIcon = newValue);
+                        setState(() => _selectedIcon = newValue);
+                      },
+                      items: availableIcons.map<DropdownMenuItem<IconData>>((IconData value) {
+                        return DropdownMenuItem<IconData>(
+                          value: value,
+                          child: Icon(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Cancel'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: Text('Add'),
+                  onPressed: () {
+                    _addHabit(titleController.text, "Some description", selectedIcon);
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+
 }
